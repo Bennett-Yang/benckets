2939d6c29c01c78f3725a999b0734e42aaf9f6a9
Bennett Yang <bennett.yang@outlook.com>
2025-03-01 18:41:05 +0800

    first commit

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "Bennett-Yang/benckets:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
